1dcb1a2c4849b097191d9a6ac8f2cba57e90b7ec
Florian Marending <florian@marending.dev>
2021-05-07 23:09:05 +0200

    Create user migration script

---
+++ migrations/20210507204732_create_user_table.sql
@@ -0,0 +1,8 @@
+-- Add migration script here
+CREATE TABLE users
+( 
+  id SERIAL PRIMARY KEY,
+  username varchar(25) UNIQUE NOT NULL,
+  password varchar(100) NOT NULL,
+  created_at BIGINT NOT NULL
+);
