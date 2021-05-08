03da81b57b102efbbf60358c583ea533f8c505ac
Florian Marending <florian@marending.dev>
2021-05-08 21:55:42 +0200

    Add balance to user table

--- migrations/20210507204732_create_user_table.sql
+++ migrations/20210507204732_create_user_table.sql
@@ -4,5 +4,6 @@ CREATE TABLE users
   id SERIAL PRIMARY KEY,
   username varchar(25) UNIQUE NOT NULL,
   password varchar(100) NOT NULL,
-  created_at BIGINT NOT NULL
+  created_at BIGINT NOT NULL,
+  balance BIGINT NOT NULL
 );
