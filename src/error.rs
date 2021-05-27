80991c296e66265cd9f30f5158de06a38a2515fb
Florian Marending <florian@marending.dev>
2021-05-27 18:26:08 +0200

    Change formatting

--- src/error.rs
+++ src/error.rs
@@ -5,29 +5,29 @@ use warp::{Rejection, Reply};
 
 #[derive(Error, Debug)]
 pub enum ApiError {
-  #[error("Database error")]
-  DBError(#[from] sqlx::Error),
+    #[error("Database error")]
+    DBError(#[from] sqlx::Error),
 
-  #[error("{0}")]
-  ViolatedAssertion(String),
+    #[error("{0}")]
+    ViolatedAssertion(String),
 }
 
 impl warp::reject::Reject for ApiError {}
 
 /// Turn rejections into appropriate status codes
 pub async fn handle_rejection(err: Rejection) -> Result<impl Reply, Rejection> {
-  if let Some(custom_error) = err.find::<ApiError>() {
-    match custom_error {
-      ApiError::DBError(db_error) => {
-        error!("Database error: {:?}", db_error);
-        return Ok(StatusCode::INTERNAL_SERVER_ERROR);
-      }
-      ApiError::ViolatedAssertion(assertion) => {
-        error!("Violated assertion: {:?}", assertion);
-        return Ok(StatusCode::INTERNAL_SERVER_ERROR);
-      }
+    if let Some(custom_error) = err.find::<ApiError>() {
+        match custom_error {
+            ApiError::DBError(db_error) => {
+                error!("Database error: {:?}", db_error);
+                return Ok(StatusCode::INTERNAL_SERVER_ERROR);
+            }
+            ApiError::ViolatedAssertion(assertion) => {
+                error!("Violated assertion: {:?}", assertion);
+                return Ok(StatusCode::INTERNAL_SERVER_ERROR);
+            }
+        }
     }
-  }
 
-  Err(err)
+    Err(err)
 }
--- src/user.rs
+++ src/user.rs
@@ -20,93 +20,92 @@ const DEFAULT_BALANCE: i64 = 5000;
 /// This request form is expected for signup and login calls.
 #[derive(Deserialize)]
 pub struct UserCredentials {
-  name: String,
-  password: String,
+    name: String,
+    password: String,
 }
 
 /// Sign up new user. This stores the user data in the db.
 pub async fn signup(
-  user: UserCredentials,
-  pool: PgPool,
+    user: UserCredentials,
+    pool: PgPool,
 ) -> Result<impl warp::Reply, warp::Rejection> {
-  info!("Creating user {}", user.name);
+    info!("Creating user {}", user.name);
 
-  if user_exists(&pool, &user.name)
-    .await
-    .map_err(|e| reject::custom(e))?
-  {
-    warn!("User {} already exists", user.name);
-    return Ok(StatusCode::CONFLICT);
-  }
+    if user_exists(&pool, &user.name)
+        .await
+        .map_err(|e| reject::custom(e))?
+    {
+        warn!("User {} already exists", user.name);
+        return Ok(StatusCode::CONFLICT);
+    }
 
-  let hashed_password = hash(user.password, BCRYPT_COST);
+    let hashed_password = hash(user.password, BCRYPT_COST);
 
-  if hashed_password.is_err() {
-    return Ok(StatusCode::INTERNAL_SERVER_ERROR);
-  }
+    if hashed_password.is_err() {
+        return Ok(StatusCode::INTERNAL_SERVER_ERROR);
+    }
 
-  let hashed_password = hashed_password.unwrap();
+    let hashed_password = hashed_password.unwrap();
 
-  let now = get_current_time();
+    let now = get_current_time();
 
-  create_user(&pool, &user.name, &hashed_password, now)
-    .await
-    .map_err(|e| reject::custom(e))?;
+    create_user(&pool, &user.name, &hashed_password, now)
+        .await
+        .map_err(|e| reject::custom(e))?;
 
-  Ok(StatusCode::OK)
+    Ok(StatusCode::OK)
 }
 
 async fn user_exists(pool: &PgPool, name: &str) -> Result<bool, ApiError> {
-  let result = query!(
-    "SELECT COUNT(id)
-    FROM users 
-    WHERE username = $1;",
-    name
-  )
-  .fetch_one(pool)
-  .await;
+    let result = query!(
+        "SELECT COUNT(id)
+        FROM users 
+        WHERE username = $1;",
+        name
+    )
+    .fetch_one(pool)
+    .await;
 
-  match result {
-    Ok(row) => {
-      if let Some(0) = row.count {
-        Ok(false)
-      } else {
-        info!("User already exists");
-        Ok(true)
-      }
+    match result {
+        Ok(row) => {
+            if let Some(0) = row.count {
+                Ok(false)
+            } else {
+                info!("User already exists");
+                Ok(true)
+            }
+        }
+        Err(error) => Err(ApiError::DBError(error)),
     }
-    Err(error) => Err(ApiError::DBError(error)),
-  }
 }
 
 async fn create_user(
-  pool: &PgPool,
-  name: &str,
-  password_hash: &str,
-  time: i64,
+    pool: &PgPool,
+    name: &str,
+    password_hash: &str,
+    time: i64,
 ) -> Result<(), ApiError> {
-  let query_result = query!(
-    "INSERT INTO users (username, password, created_at, balance)
-     VALUES ($1, $2, $3, $4);",
-    name,
-    password_hash,
-    time,
-    DEFAULT_BALANCE
-  )
-  .execute(pool)
-  .await;
+    let query_result = query!(
+        "INSERT INTO users (username, password, created_at, balance)
+        VALUES ($1, $2, $3, $4);",
+        name,
+        password_hash,
+        time,
+        DEFAULT_BALANCE
+    )
+    .execute(pool)
+    .await;
 
-  // TODO Fix !== 1 case
-  match query_result {
-    Ok(result) => {
-      if result.rows_affected() == 1 {
-        Ok(())
-      } else {
-        Err(ApiError::ViolatedAssertion(
-          "Multiple rows affected in user check".to_string(),
-        ))
-      }
+    match query_result {
+        Ok(result) => {
+            if result.rows_affected() == 1 {
+                Ok(())
+            } else {
+                Err(ApiError::ViolatedAssertion(
+                    "Multiple rows affected in user check".to_string(),
+                ))
+            }
+        }
+        Err(error) => Err(ApiError::DBError(error)),
     }
-    Err(error) => Err(ApiError::DBError(error)),
-  }
 }
