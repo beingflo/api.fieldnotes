0f6efa25b8ed0171b72cbfc3306c5aacbc987587
Florian Marending <florian@marending.dev>
2021-07-11 23:40:54 +0200

    Add notes table

---
+++ migrations/20210711204629_create_notes_table.sql
@@ -0,0 +1,12 @@
+-- Add migration script here
+CREATE TABLE notes 
+( 
+  id SERIAL PRIMARY KEY,
+  token varchar(32) NOT NULL,
+  user_id integer REFERENCES users(id) NOT NULL,
+  created_at BIGINT NOT NULL,
+  modified_at BIGINT NOT NULL,
+  title varchar(100) NOT NULL,
+  tags varchar NOT NULL,
+  content varchar NOT NULL
+);
