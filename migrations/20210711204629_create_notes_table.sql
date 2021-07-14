bf6e542b31cc9cb5e8c4bfcd98faa5b29ffb373d
Florian Marending <florian@marending.dev>
2021-07-14 22:45:33 +0200

    Add deleted_at to notes table

--- migrations/20210711204629_create_notes_table.sql
+++ migrations/20210711204629_create_notes_table.sql
@@ -6,6 +6,7 @@ CREATE TABLE notes
   user_id integer REFERENCES users(id) NOT NULL,
   created_at BIGINT NOT NULL,
   modified_at BIGINT NOT NULL,
+  deleted_at BIGINT,
   title varchar(100) NOT NULL,
   tags varchar NOT NULL,
   content varchar NOT NULL
