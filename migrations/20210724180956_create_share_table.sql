65568d578b89f4722e4f1a9c977ac9b3c36944cc
Florian Marending <florian@marending.dev>
2021-07-27 22:55:38 +0200

    Add user_id to shares table for easier querying

--- migrations/20210724180956_create_share_table.sql
+++ migrations/20210724180956_create_share_table.sql
@@ -3,5 +3,6 @@ CREATE TABLE shares
   id SERIAL PRIMARY KEY,
   token varchar(128) NOT NULL,
   note_id integer NOT NULL REFERENCES notes(id),
+  user_id integer NOT NULL REFERENCES users(id),
   created_at TIMESTAMPTZ NOT NULL
 );
