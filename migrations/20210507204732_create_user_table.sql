da6bcb6cdc50ac9c844e507afe1fa963de519c4c
Florian Marending <florian@marending.dev>
2021-09-05 12:29:17 +0200

    Add salt to db

--- migrations/20210507204732_create_user_table.sql
+++ migrations/20210507204732_create_user_table.sql
@@ -3,6 +3,7 @@ CREATE TABLE users
   id SERIAL PRIMARY KEY,
   username varchar(50) NOT NULL UNIQUE,
   password varchar(100) NOT NULL,
+  salt varchar(32),
   created_at TIMESTAMPTZ NOT NULL,
   deleted_at TIMESTAMPTZ,
   balance BIGINT NOT NULL
