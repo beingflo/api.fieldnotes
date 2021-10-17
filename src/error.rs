9962410783a7831ecf3f446e40607bc0e4733b02
Florian Marending <florian@marending.dev>
2021-10-17 21:06:17 +0200

    Remove error logging in recover

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,4 @@
-use log::error;
+use log::{error, warn};
 use thiserror::Error;
 use warp::http::StatusCode;
 use warp::reject::{InvalidHeader, MissingCookie};
@@ -27,35 +27,30 @@ impl warp::reject::Reject for ApiError {}
 /// Turn rejections into appropriate status codes
 pub async fn handle_rejection(err: Rejection) -> Result<impl Reply, Rejection> {
     if err.find::<MissingCookie>().is_some() {
-        error!("Unauthorized access");
+        warn!("Missing cookie");
         return Ok(StatusCode::UNAUTHORIZED);
     }
 
     if err.find::<InvalidHeader>().is_some() {
-        error!("Unauthorized access");
+        warn!("Invalid header");
         return Ok(StatusCode::UNAUTHORIZED);
     }
 
     if let Some(custom_error) = err.find::<ApiError>() {
         match custom_error {
-            ApiError::DBError(db_error) => {
-                error!("Database error: {:?}", db_error);
+            ApiError::DBError(_db_error) => {
                 return Ok(StatusCode::INTERNAL_SERVER_ERROR);
             }
-            ApiError::ViolatedAssertion(assertion) => {
-                error!("Violated assertion: {:?}", assertion);
+            ApiError::ViolatedAssertion(_assertion) => {
                 return Ok(StatusCode::INTERNAL_SERVER_ERROR);
             }
             ApiError::Unauthorized => {
-                error!("Unauthorized access");
                 return Ok(StatusCode::UNAUTHORIZED);
             }
             ApiError::Underfunded => {
-                error!("Underfunded account trying write");
                 return Ok(StatusCode::PAYMENT_REQUIRED);
             }
             ApiError::Conflict => {
-                error!("Conflicting write");
                 return Ok(StatusCode::CONFLICT);
             }
         }
